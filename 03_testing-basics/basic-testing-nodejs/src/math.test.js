5732ec864bc823a5e1743bd5ee8f457f9547d923
fabogit <fabo.dev.email@gmail.com>
2022-06-20 23:08:50 +0200

    03 #31
    Adding Tests To Frontend & Backend Projects

---
+++ 03_testing-basics/basic-testing-nodejs/src/math.test.js
@@ -0,0 +1,59 @@
+import { it, expect, describe } from 'vitest';
+
+import { add } from './math';
+
+describe('add()', () => {
+	it('should summarize all numbers values in an array', () => {
+		// Arrange
+		const numbers = [1, 2, 3];
+
+		// Act
+		const result = add(numbers);
+
+		// Assert
+		const expectedResult = numbers.reduce((prevValue, currValue) => prevValue + currValue, 0);
+		expect(result).toBe(expectedResult);
+	});
+
+	it('should yeld NaN if at least one invalid number is provided', () => {
+		const inputs = ['Invalid', 5];
+
+		const result = add(inputs);
+
+		expect(result).toBeNaN();
+	});
+
+	it('should yeld a correct sum if an array of numeric string values is provided', () => {
+		const stringNumbers = ['1', '2'];
+
+		const result = add(stringNumbers);
+
+		const expectedResult = stringNumbers.reduce((prevValue, currValue) => +prevValue + +currValue, 0);
+		expect(result).toBe(expectedResult);
+	});
+
+	it('should yeld 0 if an empty array is provided', () => {
+		const numbers = [];
+
+		const result = add(numbers);
+
+		expect(result).toBe(0);
+	});
+
+	it('should throw an error if no value is passed in the function', () => {
+		const resultFn = () => add();
+
+		expect(resultFn).toThrow(/is not iterable/);
+		// try/catch error catch & expect(error).toBeDefined()
+	});
+
+	it('should throw an error if provided with multiple arguments instead of an array', () => {
+		const num1 = 1;
+		const num2 = 2;
+
+		const resultFn = () => add(num1, num2);
+
+		// regex error message
+		expect(resultFn).toThrow(/is not iterable/);
+	});
+});
---
+++ 03_testing-basics/basic-testing-nodejs/src/util/numbers.test.js
@@ -0,0 +1,32 @@
+import { it, expect, describe } from 'vitest';
+
+import { transformToNumber } from './numbers';
+
+describe('transformToNumber()', () => {
+	it('should transform a string number to a number of type Number', () => {
+		const input = '5';
+
+		const result = transformToNumber(input);
+
+		expect(result).toBeTypeOf('number');
+	});
+
+	it('should transform a string number to the corresponding number of type Number', () => {
+		const input = '1';
+
+		const result = transformToNumber(input);
+
+		expect(result).toBe(+input);
+	});
+
+	it('should yeld NaN for non-transformable values', () => {
+		const input = 'invalid';
+		const input2 = {};
+
+		const result = transformToNumber(input);
+		const result2 = transformToNumber(input2);
+
+		expect(result).toBeNaN();
+		expect(result2).toBeNaN();
+	});
+});
