47a55878a5b27bc3dabb87340005c4e6c0a5e1c5
fabogit <fabo.dev.email@gmail.com>
2022-06-20 21:39:48 +0200

    03 #28
    Tests With Multiple Assertions (Multiple Expectations)

--- 03_testing-basics/basic-testing-frontend/src/util/numbers.test.js
+++ 03_testing-basics/basic-testing-frontend/src/util/numbers.test.js
@@ -3,7 +3,6 @@ import { it, expect } from 'vitest';
 import { transformToNumber } from './numbers';
 
 it('should transform a string number to a number of type Number', () => {
-
 	const input = '5';
 
 	const result = transformToNumber(input);
@@ -11,11 +10,21 @@ it('should transform a string number to a number of type Number', () => {
 	expect(result).toBeTypeOf('number');
 });
 
-it('should yeld NaN for non-transformable values', () => {
-
-	const input = 'invalid';
+it('should transform a string number to the corresponding number of type Number', () => {
+	const input = '1';
 
 	const result = transformToNumber(input);
 
+	expect(result).toBe(+input);
+});
+
+it('should yeld NaN for non-transformable values', () => {
+	const input = 'invalid';
+	const input2 = {};
+
+	const result = transformToNumber(input);
+	const result2 = transformToNumber(input2);
+
 	expect(result).toBeNaN();
+	expect(result2).toBeNaN();
 });
