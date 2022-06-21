cd474a1c3fedc9228ef7a7e12bd9c65908eb6d70
fabogit <fabo.dev.email@gmail.com>
2022-06-21 10:43:37 +0200

    06 #48
    toBe() vs toEqual()

---
+++ 06_advanced-testing-concepts/src/util/numbers.test.js
@@ -0,0 +1,54 @@
+import { it, expect, describe } from 'vitest';
+
+import { transformToNumber, cleanNumbers } from './numbers';
+
+// unit test
+describe('transformToNumber()', () => {
+	it('should transform a string number to a number of type number', () => {
+		const input = '1';
+
+		const result = transformToNumber(input);
+
+		expect(result).toBeTypeOf('number');
+	});
+
+	it('should transform a string number to a number of type number', () => {
+		const input = '1';
+
+		const result = transformToNumber(input);
+
+		expect(result).toBe(+input);
+	});
+
+	it('should yield NaN for non-transformable values', () => {
+		const input = 'invalid';
+		const input2 = {};
+
+		const result = transformToNumber(input);
+		const result2 = transformToNumber(input2);
+
+		expect(result).toBeNaN();
+		expect(result2).toBeNaN();
+	});
+});
+
+// integration test
+describe('cleanNumbers()', () => {
+	it('should return an array of numbers values if an array of string numbers values is provided', () => {
+		const numberValues = ['1', '2'];
+
+		const cleanedNumbers = cleanNumbers(numberValues);
+
+		expect(cleanedNumbers[0]).toBeTypeOf('number');
+		// use toEqual() to evaluate reference values
+		expect(cleanedNumbers).toEqual([1, 2]);
+	});
+
+	it('should throw an error if an array with at least one empty string is provided', () => {
+		const numberValues = ['', '2'];
+
+		const cleanFn = () => cleanNumbers(numberValues);
+
+		expect(cleanFn).toThrow();
+	});
+});
