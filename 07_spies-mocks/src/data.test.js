92cd93c91554f2ba880766995d56011df3f8c6bb
fabogit <fabo.dev.email@gmail.com>
2022-06-21 17:51:21 +0200

    07 #69
    More Mocking Functionalities

--- 07_spies-mocks/src/data.test.js
+++ 07_spies-mocks/src/data.test.js
@@ -5,7 +5,13 @@ import { generateReportData } from './data';
 describe('generateReportData()', () => {
 	it('should execute logFn if provided', () => {
 		// spy replacement
-		const logger = vi.fn(() => {});
+		const logger = vi.fn();
+
+		logger.mockImplementation(() => {
+			// spy method
+			// override implementation, mockImplementationOnce() only for one call
+		});
+
 		generateReportData(logger);
 		expect(logger).toBeCalled();
 	});
