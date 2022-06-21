dfa80eeaf4acce605bb37045bf3766e68260cf80
fabogit <fabo.dev.email@gmail.com>
2022-06-21 10:26:27 +0200

    05 #45
    Testing For Errors

--- 05_integration-tests/src/util/numbers.test.js
+++ 05_integration-tests/src/util/numbers.test.js
@@ -41,4 +41,12 @@ describe('cleanNumbers()', () => {
 
 		expect(cleanedNumbers[0]).toBeTypeOf('number');
 	});
+
+	it('should throw an error if an array with at least one empty string is provided', () => {
+		const numberValues = ['', '2'];
+
+		const cleanFn = () => cleanNumbers(numberValues);
+
+		expect(cleanFn).toThrow();
+	});
 });
