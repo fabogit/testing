8d23a385235e3720bd96d69ab6ff95438d7bbfa7
fabogit <fabo.dev.email@gmail.com>
2022-06-21 12:24:40 +0200

    06 #50
    Testing Asynchronous Code With Callbacks

--- 06_advanced-testing-concepts/async-code/async-example.test.js
+++ 06_advanced-testing-concepts/async-code/async-example.test.js
@@ -3,13 +3,25 @@ import { it, expect, describe } from 'vitest';
 import { generateToken } from './async-example';
 
 describe('generateToken()', () => {
-	it('should generate a token value', () => {
+	it('should generate a token value', () => new Promise(done => {
 		const testUserEmail = 'test@email.com';
 
 		generateToken(testUserEmail, (err, token) => {
 			// expecting callback result to exist
-			expect(token).toBeDefined();
-			// expect(token).toBe(2); // async bug, this also works
+			try {
+				expect(token).toBeDefined();
+				done();
+			} catch (error) {
+				done(error);
+			}
+
+			// FIX async bug, this works if done cb is not present
+			// try {
+			// 	expect(token).toBe(2);
+			// 	done();
+			// } catch (error) {
+			// 	done(error);
+			// }
 		});
-	});
+	}));
 });
