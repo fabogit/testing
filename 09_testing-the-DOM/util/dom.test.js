b48a00209745028567a1a7017fbc221a38981474
fabogit <fabo.dev.email@gmail.com>
2022-06-22 11:45:08 +0200

    09 #88
    Testing the DOM

--- 09_testing-the-DOM/util/dom.test.js
+++ 09_testing-the-DOM/util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { Window } from 'happy-dom';
 
 import { showError } from './dom';
@@ -12,13 +12,41 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-// render virtual page
-document.write(htmlDocumentContent);
-// mock the global document object
-vi.stubGlobal('document', document)
 
-describe('DOM testing', () => {
-	it('first test', () => {
-		showError('test');
-	});
-})
+// mock the global document object
+vi.stubGlobal('document', document);
+
+beforeEach(() => {
+	// rerender virtual page
+	document.body.innerHTML = '';
+	document.write(htmlDocumentContent);
+});
+
+it('should add an error paragraph to the id=errors element', () => {
+	showError('test');
+
+	const errorEl = document.getElementById('errors');
+	const errorParagraph = errorEl.firstElementChild;
+
+	expect(errorParagraph).not.toBeNull();
+});
+
+it('should not contain an error paragraph initially', () => {
+	showError('test');
+
+	const errorEl = document.getElementById('errors');
+	const errorParagraph = errorEl.firstElementChild;
+
+	expect(errorParagraph).toBeNull();
+});
+
+it('should output the provided message in the error paragraph', () => {
+	const testErrorMessage = 'Test';
+
+	showError(testErrorMessage);
+
+	const errorEl = document.getElementById('errors');
+	const errorParagraph = errorEl.firstElementChild;
+
+	expect(errorParagraph.textContent).toBe(testErrorMessage);
+});
